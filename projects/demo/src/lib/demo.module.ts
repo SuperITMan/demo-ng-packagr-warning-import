4ec44edcd4b0cf9013dce091c0cf024da5deeb93
SuperITMan <admin@superitman.com>
2021-08-24 00:45:13 +0200

    generate library

---
+++ projects/demo/src/lib/demo.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { DemoComponent } from './demo.component';
+
+
+
+@NgModule({
+  declarations: [
+    DemoComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    DemoComponent
+  ]
+})
+export class DemoModule { }
